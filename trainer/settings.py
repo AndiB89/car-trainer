dcbe6f22dee8f08fb832e10f6dbd8101bfbef8a5
AndiB89 <andreas.bopp@gmx.de>
2020-04-26 00:00:02 +0200

    postgres

--- trainer/settings.py
+++ trainer/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'os.environ.get('DB_NAME')',
         'USER': 'os.environ.get('DB_USER')',
-        'PASSWORD': 'os.environ.get('DB_NAME')',
-        'HOST': 'os.environ.get('DB_NAME')',
+        'PASSWORD': 'os.environ.get('DB_PASSWORD')',
+        'HOST': 'os.environ.get('DB_HOST')',
         'PORT': 'os.environ.get('DB_PORT')',
     }
 }
